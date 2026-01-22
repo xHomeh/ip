77ff9a3290a5afaf933335e8365b0f2f2ffc484f
xHomeh <xhomeh@gmail.com>
2026-01-23 01:39:34 +0800

    Changed DUKE to Blue

---
+++ src/main/java/Blue.java
@@ -0,0 +1,11 @@
+public class Blue {
+    public static void main(String[] args) {
+        String logo = " ____  _            \n"
+                + "|  _ \\| |             \n"
+                + "| |_) | |_   _  ___    \n"
+                + "|  _ <| | | | |/ _ \\  \n"
+                + "| |_) | | |_| |  __/   \n"
+                + "|____/|_|\\__,_|\\___| \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
