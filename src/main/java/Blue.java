878b2fa4375d997133b661655e9a71c3bb7dd872
xHomeh <xhomeh@gmail.com>
2026-01-23 02:01:36 +0800

    Added greeting and goodbye skeleton messages

--- src/main/java/Blue.java
+++ src/main/java/Blue.java
@@ -1,11 +1,28 @@
 public class Blue {
-    public static void main(String[] args) {
+    private static void greet() {
         String logo = " ____  _            \n"
                 + "|  _ \\| |             \n"
                 + "| |_) | |_   _  ___    \n"
                 + "|  _ <| | | | |/ _ \\  \n"
                 + "| |_) | | |_| |  __/   \n"
                 + "|____/|_|\\__,_|\\___| \n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        String greeting = "__________________________________________________ \n"
+                    + "Hi☆・*。It's me, Blue! \n"
+                    + "What do you need help with? \n"
+                    + "__________________________________________________ \n";
+        System.out.println(greeting);
+    }
+
+    private static void bye() {
+        String goodbye = "__________________________________________________ \n"
+                    + "Byeee~ See you soon! \n"
+                    + "__________________________________________________ \n";
+        System.out.println(goodbye);
+    }
+
+    public static void main(String[] args) {
+        greet();
     }
 }
